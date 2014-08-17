d98911c167ee3435af5b3096b63a4e5fda4b2db0
Nyitrai Jozsef <nyitrai.jozsef@gmail.com>
2014-08-18 00:11:20 +0200

    Add script that flush rules.

---
+++ flush.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo -n "Flushing iptables rules..."
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+echo 'OK'
--- portforwarding.sh
+++ portforwarding.sh
@@ -9,4 +9,6 @@ fi
 ip=$1
 port=$2
 
+echo -n "Setting portforward from port $port to $ip:$port..."
 iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $ip:$port
+echo 'OK'
